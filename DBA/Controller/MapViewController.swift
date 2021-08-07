8be2d8ff5b39864b6085060d8eff78a37744c9a7
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-08-07 03:39:50 +0100

    Add view contianing slider to alter the distance tmap renders stops when on near me view. Weather widget moved into view with grey background and rounded corners.

--- DBA/Controller/MapViewController.swift
+++ DBA/Controller/MapViewController.swift
@@ -19,34 +19,51 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     var nearMeChosen = false
     
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var nearMeControlsView: UIView!
+    
+    @IBOutlet weak var legalButton: UIButton!
+    @IBOutlet weak var locationButton: UIButton!
+    
+    @IBOutlet weak var distanceSlider: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    var userLocation: CLLocationCoordinate2D?
+    var radius: Double = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        GMSServices.provideAPIKey(S.googleMapsAPIKey)
-        
-        
+        roundCorners(buttons)
         
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
+        
+        
+        
         // Do any additional setup after loading the view.
         
         
         
     }
     
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else {
             return
         }
+        
+        
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let coordinate = location.coordinate
-        let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 16.0)
+        let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 13.0)
         //let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
         mapView.camera = camera
+        mapView.settings.compassButton = true
+        
+        
         mapView.isMyLocationEnabled = true
         
         
@@ -57,19 +74,24 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        let userLocation = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
-        marker.position = userLocation
+        userLocation = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        marker.position = userLocation!
         marker.title = "You Are Here"
         marker.snippet = ":)"
         marker.map = mapView
-        marker.icon = UIImage(systemName: "figure.wave.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 40))
+        //marker.icon = UIImage(systemName: "figure.wave.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 40))
+        mapView.animate(toViewingAngle: 45)
+              
+              
         locationManager.stopUpdatingLocation()
         
         if nearMeChosen {
-            generateStopsNearMePins(mapView, userLocation)
+            generateStopsNearMePins()
         } else {
             generateRouteStopPins(mapView)
         }
+        
+        
     }
     
     //    override func viewDidAppear(_ animated: Bool) {
@@ -95,9 +117,9 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    func generateStopsNearMePins(_ mapView: GMSMapView, _ userLocation: CLLocationCoordinate2D) {
-        
-        let region = CLCircularRegion(center: userLocation, radius: 1000.0, identifier: "RegionID")
+    func generateStopsNearMePins() {
+        mapView.clear()
+        let region = CLCircularRegion(center: userLocation!, radius: radius, identifier: "RegionID")
         for stop in K.stopsLocations {
             if region.contains(CLLocationCoordinate2D(latitude: stop.lat, longitude: stop.long)) {
                 let stopMarker = GMSMarker()
@@ -109,12 +131,53 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
     }
+  
     
 }
 
 
+//MARK: - Buttons
+
+extension MapViewController {
+    
+    
+    @IBAction func legalButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: K.map.legal, sender: self)
+    }
+    
+    
+    
+    @IBAction func viewControllerPressed(_ sender: UIButton) {
+        if nearMeChosen {
+            nearMeControlsView.alpha = 1
+        }
+    }
+    
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+        locationManager.startUpdatingLocation()
+    }
+    
+}
 
 
+//MARK: - Near Me Controls
+
+extension MapViewController {
+    
+    @IBAction func hideNearMeControls(_ sender: UIButton) {
+        nearMeControlsView.alpha = 0
+    }
+    
+    @IBAction func sliderDidSlide(_ sender: UISlider) {
+        let value = sender.value
+        sliderLabel.text = String(format: "Showing stops within %.2fkm", (value/4))
+        
+        radius = Double(value*250)
+        
+        generateStopsNearMePins()
+    }
+    
+}
 
 
 //MARK: - Audio Book Control
